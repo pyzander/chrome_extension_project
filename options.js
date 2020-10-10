8070d7f85c13074d640fa65a62dac0cbd1555863
agong3 <agong3@illinois.edu>
2020-10-10 16:23:12 -0500

    fixing variables. stick can't display diff options

--- options.js
+++ options.js
@@ -6,6 +6,8 @@
 
 'use strict';
 
+let page = document.getElementById('buttonDiv');
+
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 
 function constructOptions(kButtonColors) {
@@ -17,7 +19,7 @@ function constructOptions(kButtonColors) {
         console.log('color is ' + item);
       })
     });
-    page.appendChild(button);
+    page.appendChild(button); 
   }
 }
 constructOptions(kButtonColors);
